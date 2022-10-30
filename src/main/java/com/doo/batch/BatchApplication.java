6cf0558eff0e8d26d6a1ea16da96663edba88bb7
doo <gostbaducking1@nate.com>
2022-10-30 18:16:29 +0900

    feat: 배치, 스케쥴링 enabled, 서버 기동시 배치 실행 막음

--- src/main/java/com/doo/batch/BatchApplication.java
+++ src/main/java/com/doo/batch/BatchApplication.java
@@ -1,13 +1,18 @@
 package com.doo.batch;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
+@EnableBatchProcessing
 @SpringBootApplication
 public class BatchApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BatchApplication.class, args);
+		// SpringApplication.run(BatchApplication.class, args);
+		new SpringApplication(BatchApplication.class).run(args);
 	}
 	
 }
